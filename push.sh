e2c16ee0027ed406647bc66f8f3a4063aee5a0ce
Oleg Dreyman <dreymonde@me.com>
2017-06-10 19:14:22 +0300

    New edition

--- push.sh
+++ push.sh
@@ -5,16 +5,18 @@ mkdir complication-pushes
 
 swift build --chdir ../Generator/
 ../Generator/.build/debug/Generator "$@"
-git add -A
-git commit -m "New edition"
-git push origin master
 
 cd complication-pushes
 
 for file in *.json
 do
-    echo $file
-    ruby ../../Pusher/pusher.rb "$file"
+echo $file
+ruby ../../Pusher/pusher.rb "$file"
 done
 
 cd ..
+
+git add -A
+git commit -m "New edition"
+git push origin master
+
